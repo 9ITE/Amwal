8dca986d87c7cb67818a88abbb920f91cb9e1d8c
Ankur Bisht <ankur.b@kodehash.com>
2024-10-08 15:12:50 +0530

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+import { Suspense } from "react";
+import Layout from "./Pages/Layout";
+
+const Dashboard = lazy(() => import("./Pages/Dashboard"));
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
