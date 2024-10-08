9125013f175cd5df1d84557530761dae2a7b6c5b
Ankur Bisht <ankur.b@kodehash.com>
2024-10-08 15:12:05 +0530

    tailwind & font-family setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          200: "#03a6ff",
+        },
+        gray: { 100: "#f7f9f9", 600: "#243640" },
+        yellow: { 500: "#f2b818", 600: "#f59b42" },
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        concert: ["Concert One", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
